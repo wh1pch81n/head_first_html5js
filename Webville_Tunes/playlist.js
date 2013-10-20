5ef004c5283542b5a1d1043f33065a2642f5c3aa
wh1pch81n <wh1pch81n@gmail.com>
2013-10-20 16:34:09 -0700

    the entered song names can get saved

--- Webville_Tunes/playlist.js
+++ Webville_Tunes/playlist.js
@@ -8,13 +8,16 @@ function handleButtonClick () {
 		var li_element = document.createElement("li");
 		li_element.innerHTML = songName;
 		document.getElementById("playlist").appendChild(li_element);
+		save(songName);
 		textInput.value = "";
+		
 	}
 }
 
 function init() {
 	var button = document.getElementById("addButton");
 	button.onclick = handleButtonClick;
+	loadPlaylist();
 }
 
 window.onload = init;
