948bef92c60e656658079f0606fc307bd1dd2ef7
wh1pch81n <wh1pch81n@gmail.com>
2013-10-20 16:07:59 -0700

    textinput is cleared after sumbit

--- Webville_Tunes/playlist.js
+++ Webville_Tunes/playlist.js
@@ -1,10 +1,14 @@
 function handleButtonClick () {
-	var textInput = document.getElementById("songTextInput")
+	var textInput = document.getElementById("songTextInput");
 	var songName = textInput.value;
 	if (songName == "") {
 		alert("Please enter a song name then submit");
 	} else {
 		alert("Adding " + songName);
+		var li_element = document.createElement("li");
+		li_element.innerHTML = songName;
+		document.getElementById("playlist").appendChild(li_element);
+		textInput.value = "";
 	}
 }
 
