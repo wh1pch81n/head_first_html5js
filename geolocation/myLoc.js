ea4c336ba204f538ab722d380599c62d8242d8a9
wh1pch81n <wh1pch81n@gmail.com>
2013-10-21 17:16:54 -0700

    added an error handler

--- geolocation/myLoc.js
+++ geolocation/myLoc.js
@@ -4,8 +4,8 @@ function getMyLocation() {
 	/*check if geolocation is supported on user's browser*/
 	if( navigator.geolocation) {
 		/*displayLocation is a function pointer.  the getCurrentPosition() will call it with the arguments of the position.  What it does with the position will depend on the implementation of displayLocation()*/
-		navigator.geolocation.getCurrentPosition(displayLocation,);
-		
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+		/*additionally we can add an error handeler*/
 	} else {
 		alert("Oops, no geo location support");
 	}
@@ -19,4 +19,23 @@ function displayLocation(position) {
 	div.innerHTML = "Your location is: <br>"
 	+ "Latitude: " + latitude + "<br>"
 	+ "Longitude: " + longitude + "<br>";
+}
+
+function displayError(error) {
+	//this is how we make static objects...but we are using it as a hybrid of enum and array.
+	var errorTypes = {
+		0: "Unknown error",
+		1: "Permission denied by user",
+		2: "Position is not available",
+		3: "Request timed out"
+	}
+	
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		/*error DOT message provides additional info for the error*/
+		errorMessage += " " + error.message;
+	}
+	
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
 }
