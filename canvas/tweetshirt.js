b76ecaa84c167ca46dc6a60536141c3e428e4c0c
wh1pch81n <wh1pch81n@gmail.com>
2013-10-29 16:46:24 -0700

    adding random squares

--- canvas/tweetshirt.js
+++ canvas/tweetshirt.js
@@ -1,19 +1,19 @@
 window.onload = function() {
-	var previewButtom = document.getElementById("previewButton");
-	button.onclick = previewHandler;
+	var previewButton = document.getElementById("previewButton");
+	previewButton.onclick = previewHandler;
 	
-	var canvas = document.getElementById("tShirtCanvas");
-	if (canvas.getContext == null) return;
-	var context = canvas.getContext("2d"); //you need a 2d context from canvas in order to draw
-
-	var xPos = 10;
-	var xLen = 100;
-	var yPos = 10;
-	var yLen = 100;
-	var blackRect = context.fillRect( xPos, yPos, xLen, yLen);
-	
-	var emptyRect = context.strokeRect( xPos + 200, yPos, xLen, yLen);
-	
+//	var canvas = document.getElementById("tShirtCanvas");
+//	if (canvas.getContext == null) return;
+//	var context = canvas.getContext("2d"); //you need a 2d context from canvas in order to draw
+//
+//	var xPos = 10;
+//	var xLen = 100;
+//	var yPos = 10;
+//	var yLen = 100;
+//	var blackRect = context.fillRect( xPos, yPos, xLen, yLen);
+//	
+//	var emptyRect = context.strokeRect( xPos + 200, yPos, xLen, yLen);
+//	
 //	context.setPixel(400, yPos);
 	//notice that the fillRect doesn't have color... more later
 	
@@ -26,11 +26,25 @@ function previewHandler() {
 	
 	var selectObj = document.getElementById("shape");
 	var index = selectObj.selectedIndex;
-	var shape = selectedObj[index].value;
+	var shape = selectObj[index].value;
 	
 	if( shape == "squares") {
 		for (var squares = 0; squares < 20; squares++) {
 			drawSquare( canvas, context);
 		}
 	}
+}
+
+function drawSquare( canvas, context) {
+	//caluclate random y position for square
+	var y = Math.floor(Math.random() * canvas.height);
+	//calculate random x position for square
+	var x = Math.floor(Math.random() * canvas.width);
+	//calculate random width for square
+	var w = Math.floor(Math.random() * 20);
+	//set the fillstyle to light blue
+	context.fillstyle = "lightblue";
+	//draw a square at position x, y, with width w
+	context.fillRect(x, y, w, w);
+	
 }
