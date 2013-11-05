d3e3d95043b4ff882526df463de61c1213ad5193
wh1pch81n <wh1pch81n@gmail.com>
2013-11-04 17:58:19 -0800

    add comments

--- canvas/tweetshirt.js
+++ canvas/tweetshirt.js
@@ -1,23 +1,7 @@
 window.onload = function() {
 	var previewButton = document.getElementById("previewButton");
 	previewButton.onclick = previewHandler;
-	
-//	var canvas = document.getElementById("tShirtCanvas");
-//	if (canvas.getContext == null) return;
-//	var context = canvas.getContext("2d"); //you need a 2d context from canvas in order to draw
-//
-//	var xPos = 10;
-//	var xLen = 100;
-//	var yPos = 10;
-//	var yLen = 100;
-//	var blackRect = context.fillRect( xPos, yPos, xLen, yLen);
-//	
-//	var emptyRect = context.strokeRect( xPos + 200, yPos, xLen, yLen);
-//	
-//	context.setPixel(400, yPos);
-	//notice that the fillRect doesn't have color... more later
-	
-	
+	//alert(document.URL);
 };
 
 function previewHandler() {
